b8ed40879fbbe186a810e2c1a4c2a5be522924ab
baxnul <khafizovilnaz100@gmail.com>
2024-01-02 23:46:19 +0300

    Добавлена возможность проверки элемента на странице. 1) В файле "base_page.py", добавлена функция "is_element_present" для проверки наличия элемента, результат True/False 2) В "main_page.py"-> класс "MainPage" добавлена функция "should_be_login_link" 3) В файл "test_main_page.py", добавлен тест-кейс "test_guest_should_see_login_link"

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,19 @@
+from selenium.common.exceptions import *
+
+
 class BasePage:
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, by, locator):
+        """Find an element given a By strategy and locator"""
+        try:
+            self.browser.find_element(by, locator)
+        except NoSuchElementException:
+            return False
+        return True
